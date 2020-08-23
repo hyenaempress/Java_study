c9a0d34cf9aa9009ea02c17ffac67fec90fb5b5f
hyenaenpress <catnaro16@gmail.com>
2020-08-23 16:45:30 +0900

    배열

--- day04/src/ReturnArray.java
+++ day04/src/ReturnArray.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class ReturnArray {
+
+    static int[]makeArray(){//정수형 배열을 리턴하는 메소드
+        int temp[] =new int[4];//배열 생성
+        for (int i=0; i<temp.length; i++)
+            temp[i] =i;
+        return temp; //배열 리턴
+    }
+
+    public static void main(String[] args) {
+        int intArray[]; //배열 레퍼런스 변수 선언
+        intArray = makeArray();//메소드로 부터 배열 전달 받음
+        for (int i=0; i<intArray.length; i++)
+            System.out.println(intArray[i]+" "); //배열 모두 출력
+
+    }
+
 }
--- day04/src/SkewedArray.java
+++ day04/src/SkewedArray.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class SkewedArray {
+
+    public static void main(String[] args) {
+        int intArray[][]= new int[4][];//각 행의 래퍼런스 배열 생성
+        intArray[0] = new int[3];
+        intArray[1] = new int[2];
+        intArray[2] = new int[3];
+        intArray[3] = new int[2];
+
+        for (int i=0; i<intArray.length; i++)
+            for (int j=0; j<intArray.length; j++) //열의 대한 반복
+                intArray[i][j] = (i+1)*10 +j;
+
+            for (int i=0; i<intArray.length; i++){
+                for (int j=0; j<intArray[i].length; j++)
+                    System.out.println(intArray[i][j]+ " ");
+                System.out.println();//다음줄로 넘어가기
+            }
+
+    }
 }
